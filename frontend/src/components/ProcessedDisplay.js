0df2a12694d93117096aa06329565f102134ee64
RielMorales <rmmorales1@up.edu.ph>
2025-07-15 15:51:33 +0800

    change interval to 550 | change processedDisplay w and h to auto

--- frontend/src/components/ProcessedDisplay.js
+++ frontend/src/components/ProcessedDisplay.js
@@ -5,7 +5,7 @@ const ProcessedDisplay = ({ processedUrl }) => {
     <div>
       <h1>Processed AR Video</h1>
       {processedUrl ? (
-        <img src={processedUrl} alt="Processed Frame" width="640" height="480" objectit="cover" />
+        <img src={processedUrl} alt="Processed Frame" width="auto" height="auto" objectit="cover" />
       ) : (
         <p>Waiting for video...</p>
       )}

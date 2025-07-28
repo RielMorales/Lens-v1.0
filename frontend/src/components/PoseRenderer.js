2be5c30757533422550e6eeeede96f936717a994
RielMorales <rmmorales1@up.edu.ph>
2025-07-28 16:14:18 +0800

    adjusted the video rotation

--- frontend/src/components/PoseRenderer.js
+++ frontend/src/components/PoseRenderer.js
@@ -83,8 +83,8 @@ export default function PoseRenderer({ poses }) {
 
       mesh.position.set(
         smoothedTvec[0] * scaleFactor + 0.2,
-        -smoothedTvec[1] * scaleFactor - 0.5,
-        -smoothedTvec[2] * scaleFactor - 2.0
+        -smoothedTvec[1] * scaleFactor - 0.0,
+        -smoothedTvec[2] * scaleFactor - 0.0
       )
 
       const theta = Math.sqrt(rvec[0] ** 2 + rvec[1] ** 2 + rvec[2] ** 2)

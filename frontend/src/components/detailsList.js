bb0609533c3bfe6e8ab4191b27af912ae446ae58
sbsandiego <sbsandiego@up.edu.ph>
2025-07-29 14:19:37 +0800

    modified details (year)

--- frontend/src/components/detailsList.js
+++ frontend/src/components/detailsList.js
@@ -2,6 +2,7 @@ const FullDetails = [
     {
         id: 1,
         title: 'Oblation Plaza',
+        year: '2005',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/upou-oblation.png')
     },
@@ -9,6 +10,7 @@ const FullDetails = [
     {
         id: 2,
         title: 'Main Building',
+        year: '1995',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/upou-main-building.png')
     },
@@ -16,6 +18,7 @@ const FullDetails = [
     {
         id: 3,
         title: 'IMDPO Building',
+        year: '2007',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/img-placeholder.jpg')
     },
@@ -23,6 +26,7 @@ const FullDetails = [
     {
         id: 4,
         title: 'Teaching and Learning Hub',
+        year: '2017',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/img-placeholder.jpg')
     },
@@ -30,6 +34,7 @@ const FullDetails = [
     {
         id: 5,
         title: 'World Class Multimedia Center',
+        year: '2011',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/upou-wcmc.png')
     },
@@ -37,6 +42,7 @@ const FullDetails = [
     {
         id: 6,
         title: 'CCDL Auditorium',
+        year: '2015',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/upou-ccdl.png')
     },
@@ -44,6 +50,7 @@ const FullDetails = [
     {
         id: 7,
         title: 'ICC Building',
+        year: '2022',
         description: "The UPOU International Convention Center (ICC) is a state-of-the-art facility located on the University of the Philippines Open University (UPOU) campus in Los Baños, Laguna. Officially inaugurated in 2017, the ICC serves as a premier venue for hosting international and national conferences, seminars, and events, particularly those focused on open and distance e-learning. The center is equipped with advanced telecommunications and multimedia facilities to support a wide range of academic and professional gatherings. The ICC's design emphasizes accessibility and functionality, featuring a 700-seater convention hall, 25 executive rooms, and various breakout meeting rooms of different sizes. These spaces are designed to accommodate diverse event formats and provide a conducive environment for learning and collaboration. The center's establishment underscores UPOU's commitment to enhancing the quality of education and fostering global partnerships in the field of distance education. In addition to its primary function as a conference venue, the ICC also plays a significant role in UPOU's broader mission to promote lifelong learning and community engagement. It serves as a hub for various academic and cultural events, contributing to the university's vibrant academic community.  For more information about the UPOU ICC and its facilities, you can visit the official UPOU website or contact the university directly.",
         image: require('../assets/images/img-placeholder.jpg')
     },
@@ -51,6 +58,7 @@ const FullDetails = [
     {
         id: 8,
         title: 'Academic Residences',
+        year: '2008',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         image: require('../assets/images/img-placeholder.jpg')
     },

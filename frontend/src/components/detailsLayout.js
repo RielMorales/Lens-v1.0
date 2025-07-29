d90d8d241a6047496b546e03556bbab9640376b2
RielMorales <rmmorales1@up.edu.ph>
2025-07-29 15:58:14 +0800

    added comments to detailsLayout.js

--- frontend/src/components/detailsLayout.js
+++ frontend/src/components/detailsLayout.js
@@ -1,8 +1,10 @@
+// Import React and the Navbar component
 import React from 'react';
 import Navbar from './navbar.js';
 
 import '../styles/detailsLayout.css';
 
+// Layout component used to wrap details pages
 const DetailsLayout = ({ children }) => {
     return (
         <div className="layout-details">

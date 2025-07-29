2470c446eed79b3743b2dcef63b30c3fede0ac8a
RielMorales <rmmorales1@up.edu.ph>
2025-07-29 15:57:00 +0800

    added comment to carousel.js

--- frontend/src/components/carousel.js
+++ frontend/src/components/carousel.js
@@ -1,16 +1,19 @@
+// Import React and required components from react-bootstrap
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/carousel.css';
+import { Carousel } from 'react-bootstrap';     // Carousel component from Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap CSS
+import '../styles/carousel.css';                // Custom CSS for the carousel
 
-// import images
+// Import image assets used in the carousel
 import MainBldg from '../assets/images/upou-main-building.png';
 import Oblation from '../assets/images/upou-oblation.png';
 import UPOUMap from '../assets/images/upou-map.png';
 import WCMC from '../assets/images/upou-wcmc.png';
 import CCDL from '../assets/images/upou-ccdl.png';
 
+// Functional component to render the image carousel
 function CarouselImage() {
+    // Array of image sources to display in the carousel
     const images = [UPOUMap, Oblation, MainBldg, WCMC, CCDL];
 
     return (
